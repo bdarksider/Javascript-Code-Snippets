7969880290fc87c2f3e4a3abd8698e44c7b6cbf3
Bejoy Mathew <bejoygm@gmail.com>
2017-03-23 19:41:13 +0530

    added comparator

---
+++ common/comparator.js
@@ -0,0 +1,9 @@
+function notificationComparator(compareTo, compareFrom) {
+    for (var i = 0; i < compareFrom.length; i++) {
+        for (var j = 0; j < compareTo.length; j++) {
+            if (/* checks */) {
+                // actions
+            }
+        }
+    }
+}
